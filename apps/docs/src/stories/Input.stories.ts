00b69f50465445dc5016ba7d8ea79f523235795b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-02 14:39:26 -0300

    ♻️ refactor: move Input para a categoria Moléculas no Storybook
    Altera a organização do Storybook, movendo o componente **Input** da categoria *Example* para *Moléculas*.

--- apps/docs/src/stories/Input.stories.ts
+++ apps/docs/src/stories/Input.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 import { Input } from '@gabriel_veroneze/alfabit-input'
 
 const meta = {
-    title: 'Example/Input',
+    title: 'Moléculas/Input',
     component: Input,
     parameters: {
         layout: 'centered',
@@ -12,6 +12,7 @@ const meta = {
 } satisfies Meta<typeof Input>
 
 export default meta
+
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
