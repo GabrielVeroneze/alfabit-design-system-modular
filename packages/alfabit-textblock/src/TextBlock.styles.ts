b7477764bb79c34f951e4ea4efe2b22e3f9b9bcf
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-06 15:21:39 -0300

    ✨ feat: adiciona estilos avançados e tokens ao TextBlock
    Atualiza o componente adicionando suporte aos tokens de cor do Design System, novos elementos estruturais (Title e Description) e melhorias
    de layout como background, espaçamento e tipografia.

--- packages/alfabit-textblock/src/TextBlock.styles.ts
+++ packages/alfabit-textblock/src/TextBlock.styles.ts
@@ -1,6 +1,25 @@
+import { background, gray } from '@gabriel_veroneze/alfabit-tokens'
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
+    background: ${background.dark};
     border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
     padding: 32px 48px;
+`
+
+export const Title = styled.h2`
+    color: ${gray.primary};
+    font-size: 24px;
+    font-weight: 800;
+    line-height: 30px;
+`
+
+export const Description = styled.p`
+    color: ${gray.primary};
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
 `
