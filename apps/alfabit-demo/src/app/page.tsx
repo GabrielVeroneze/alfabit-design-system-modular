43a6efd0d2a6e668775f9b59ebac910aeeda4176
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-08 15:45:28 -0300

    ✨ feat: adiciona alternância de tema na página inicial
    Inclui os temas claro e escuro no componente, adicionando estado para controlar o tema ativo e permitindo alternar entre eles através de um novo botão. O componente `Button` agora recebe o tema atual, refletindo a mudança visual de acordo com a seleção do usuário.

--- apps/alfabit-demo/src/app/page.tsx
+++ apps/alfabit-demo/src/app/page.tsx
@@ -1,15 +1,28 @@
 'use client'
 
+import { useState } from 'react'
 import { Button } from '@gabriel_veroneze/alfabit-button'
+import { darkTheme, lightTheme, Theme } from '@gabriel_veroneze/alfabit-tokens'
 import styles from './page.module.css'
 
 export default function Home() {
+    const [appTheme, setAppTheme] = useState<Theme | undefined>(undefined)
+
+    const toggleTheme = () => {
+        setAppTheme(
+            appTheme === lightTheme || !appTheme ? darkTheme : lightTheme,
+        )
+    }
+
     return (
         <div className={styles.page}>
             <main className={styles.main}>
                 <div className={styles.intro}>
                     <h1>Bem-vindo ao Design System Alfabit</h1>
-                    <Button>Hello World, Alfabit npm!</Button>
+                    <Button theme={appTheme}>Hello World, Alfabit npm!</Button>
+                    <Button variant="secondary" onClick={toggleTheme}>
+                        Trocar o tema
+                    </Button>
                 </div>
             </main>
         </div>
