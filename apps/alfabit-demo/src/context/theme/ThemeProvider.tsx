6b54b2ff1612a2cab6f24605ef7c9cf5555abb26
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-08 16:28:45 -0300

    ✨ feat: cria ThemeProvider para gerenciar e distribuir o tema global
    Implementa o `ThemeProvider`, responsável por controlar o estado do tema da aplicação e disponibilizá-lo através do `ThemeContext`. Essa estrutura permite alterar o tema dinamicamente e garante que todos os componentes descendentes tenham acesso às informações e funções de atualização do tema.

---
+++ apps/alfabit-demo/src/context/theme/ThemeProvider.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useState } from 'react'
+import { Theme } from '@gabriel_veroneze/alfabit-tokens'
+import { ThemeContext } from './ThemeContext'
+
+interface ThemeProviderProps {
+    children: React.ReactNode
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+    const [appTheme, setAppTheme] = useState<Theme | undefined>(undefined)
+
+    const changeTheme = (theme: Theme) => {
+        setAppTheme(theme)
+    }
+
+    return (
+        <ThemeContext.Provider
+            value={{ theme: appTheme, changeTheme: changeTheme }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    )
+}
